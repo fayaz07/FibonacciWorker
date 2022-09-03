23ac96a6a08ae697d5ab3c53adb455af0ffa246c
Mohammad Fayaz <fayazfz07@gmail.com>
2022-09-04 00:00:27 +0530

    finish implementation of Fibonacci worker

--- app/src/main/java/com/mohammadfayaz/fibonacciworker/FibonacciWorker.kt
+++ app/src/main/java/com/mohammadfayaz/fibonacciworker/FibonacciWorker.kt
@@ -1,12 +1,49 @@
 package com.mohammadfayaz.fibonacciworker
 
 import android.content.Context
+import android.os.Environment
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.io.File
+import java.io.InputStream
 
 class FibonacciWorker(appContext: Context, workerParams: WorkerParameters):
        Worker(appContext, workerParams) {
    override fun doWork(): Result {
-       return Result.success()
+
+     Log.d("worker", "work started")
+
+     val count = inputData.getInt("count", 99)
+     val outputFile = File(
+       applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.path!! + "/",
+       "result.txt"
+     )
+
+     Log.d("worker", outputFile.path)
+
+     val stringBuffer: StringBuffer = StringBuffer("")
+
+     var a = 0
+     var b = 1
+     var c = a + b
+
+     stringBuffer.append(a).append(", ").append(b).append(", ")
+
+     for (i in 0..count) {
+        c = a + b
+       stringBuffer.append(c).append(", ")
+       a = b
+       b = c
+     }
+     outputFile.copyInputStreamToFile(stringBuffer.toString().byteInputStream())
+     Log.d("worker", "work ended")
+     return Result.success()
    }
+}
+
+fun File.copyInputStreamToFile(inputStream: InputStream) {
+  this.outputStream().use { fileOut ->
+    inputStream.copyTo(fileOut)
+  }
 }
--- app/src/main/java/com/mohammadfayaz/fibonacciworker/MainActivity.kt
+++ app/src/main/java/com/mohammadfayaz/fibonacciworker/MainActivity.kt
@@ -2,10 +2,33 @@ package com.mohammadfayaz.fibonacciworker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    setupWorker()
+  }
+
+  private fun setupWorker() {
+    val data = Data.Builder()
+    data.putInt("count", 99)
+
+    val fibonacciWorkerRequest: WorkRequest =
+      OneTimeWorkRequestBuilder<FibonacciWorker>()
+        .setInputData(data.build())
+        .build()
+
+    WorkManager
+      .getInstance(this)
+      .enqueue(fibonacciWorkerRequest)
+
   }
 }

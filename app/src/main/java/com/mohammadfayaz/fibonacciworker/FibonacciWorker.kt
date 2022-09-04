b111de45dd94854fb0dbf0f65fe5b0abf36ea8d8
Mohammad Fayaz <fayazfz07@gmail.com>
2022-09-04 08:31:23 +0530

    add viewbinding, write file using FileWriter for efficient performance

--- app/src/main/java/com/mohammadfayaz/fibonacciworker/FibonacciWorker.kt
+++ app/src/main/java/com/mohammadfayaz/fibonacciworker/FibonacciWorker.kt
@@ -3,14 +3,19 @@ package com.mohammadfayaz.fibonacciworker
 import android.content.Context
 import android.os.Environment
 import android.util.Log
+import androidx.work.CoroutineWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.FileWriter
 import java.io.InputStream
+import java.math.BigInteger
 
 class FibonacciWorker(appContext: Context, workerParams: WorkerParameters):
-       Worker(appContext, workerParams) {
-   override fun doWork(): Result {
+       CoroutineWorker(appContext, workerParams) {
+   override suspend fun doWork(): Result {
 
      Log.d("worker", "work started")
 
@@ -22,28 +27,35 @@ class FibonacciWorker(appContext: Context, workerParams: WorkerParameters):
 
      Log.d("worker", outputFile.path)
 
-     val stringBuffer: StringBuffer = StringBuffer("")
+     var ba: BigInteger = BigInteger.ZERO
+     var bb: BigInteger = BigInteger.ONE
+     var bc: BigInteger = ba.add(bb)
 
-     var a = 0
-     var b = 1
-     var c = a + b
+     withContext(Dispatchers.IO) {
 
-     stringBuffer.append(a).append(", ").append(b).append(", ")
+       val fw = FileWriter(
+         applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.path!!
+             + "/result.txt",
+         true)
+       fw.append("$ba, $bb, ")
 
-     for (i in 0..count) {
-        c = a + b
-       stringBuffer.append(c).append(", ")
-       a = b
-       b = c
+       for (i in 0..count) {
+         bc = ba.add(bb)
+         ba = bb
+         bb = bc
+
+         fw.append("$bc, ")
+       }
+
      }
-     outputFile.copyInputStreamToFile(stringBuffer.toString().byteInputStream())
+
      Log.d("worker", "work ended")
      return Result.success()
    }
 }
 
-fun File.copyInputStreamToFile(inputStream: InputStream) {
-  this.outputStream().use { fileOut ->
-    inputStream.copyTo(fileOut)
-  }
-}
+//fun File.copyInputStreamToFile(inputStream: InputStream) {
+//  this.outputStream().use { fileOut ->
+//    inputStream.copyTo(fileOut)
+//  }
+//}
--- app/src/main/java/com/mohammadfayaz/fibonacciworker/MainActivity.kt
+++ app/src/main/java/com/mohammadfayaz/fibonacciworker/MainActivity.kt
@@ -3,23 +3,32 @@ package com.mohammadfayaz.fibonacciworker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewbinding.ViewBinding
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
+import com.mohammadfayaz.fibonacciworker.databinding.ActivityMainBinding
 import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+
+  lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
 
-    setupWorker()
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.startWork.setOnClickListener {
+      setupWorker()
+    }
   }
 
   private fun setupWorker() {
     val data = Data.Builder()
-    data.putInt("count", 99)
+    data.putInt("count", 50000)
 
     val fibonacciWorkerRequest: WorkRequest =
       OneTimeWorkRequestBuilder<FibonacciWorker>()
